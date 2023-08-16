3a2b2fde5d4a6a84988a109bcbe01faa005f7253
Manh Quan <manhquan.05012002@gmail.com>
2023-08-16 13:54:55 +0700

    config tailwind, prettier, router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider } from 'react-router'
+import router from './routers/inex.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App>{}</App>
+    <App>
+      <RouterProvider router={router} />
+    </App>
   </React.StrictMode>
 )
