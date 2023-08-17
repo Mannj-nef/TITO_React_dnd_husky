a8644af23b9c97ed119425a293994c6ac919d023
Manh Quan <manhquan.05012002@gmail.com>
2023-08-17 14:54:23 +0700

    fix issues component button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 import { HTMLAttributes, ReactNode } from 'react'
-import { Link } from 'react-router-dom'
 
 type type = 'primary' | 'secondary' | (string & {})
 
@@ -10,15 +9,9 @@ interface IButton extends HTMLAttributes<HTMLButtonElement> {
   href?: string
 }
 
-const classCommon = 'py-2 px-12 outline-none border rounded-full block flex items-center justify-center'
-
 const Button = ({ children, className = 'button-primary', type = '', href, ...props }: IButton) => {
-  return href ? (
-    <Link to={href} className={`${classCommon} button-${type} ${className}`}>
-      {children}
-    </Link>
-  ) : (
-    <button className={` ${classCommon} button-${type} ${className}`} {...props}>
+  return (
+    <button className={`btn button-${type} ${className}`} {...props}>
       {children}
     </button>
   )
