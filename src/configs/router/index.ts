f98461059239501a2fdcff59d24be3a43db6c5c9
Manh Quan <manhquan.05012002@gmail.com>
2023-08-31 20:10:48 +0700

    config router

---
+++ src/configs/router/index.ts
@@ -0,0 +1,30 @@
+const ROUTER = {
+  HOME: {
+    path: '/',
+    name: 'Home'
+  },
+  PROFILE: {
+    path: '/profile',
+    name: 'Profile'
+  },
+  WORKSPACE: {
+    path: '/workspace',
+    name: 'Workspace',
+    DETAIL: '/workspace/:project'
+  },
+  CALENDAR: {
+    path: '/calendar',
+    name: 'Calendar'
+  },
+  SETTING: {
+    path: '/setting',
+    name: 'Setting'
+  },
+  LOGIN: {
+    path: '/login',
+    SIGN_IN: '/login/sign-in',
+    SIGN_UP: '/login/sign-up'
+  }
+} as const
+
+export default ROUTER
