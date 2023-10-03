c2efc9acc0503bbaabb4ea5a31ebaac95306c17e
Manh Quan <manhquan.05012002@gmail.com>
2023-10-03 19:00:05 +0700

    update module login

--- src/layouts/Login/index.tsx
+++ src/layouts/Login/index.tsx
@@ -1,33 +1,46 @@
+import { useEffect } from 'react'
 import { Outlet } from 'react-router'
-
-import imageBanner from '~/assets/images/banner-login-todo.jpg'
-
 import ROUTER from '~/configs/router'
-import { OuthLogin } from '~/modules/Login'
-import { Link } from 'react-router-dom'
+import { OauthLogin } from '~/modules/Login'
+import { Link, useNavigate } from 'react-router-dom'
+import { IMAGE_BANNER_LOGIN } from '~/mocks/images'
+import { getToken } from '~/utils/handleToken'
 
 const LoginLayout = () => {
+  const { token } = getToken()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!token) return
+    navigate(ROUTER.HOME.path)
+  }, [token])
+
+  if (token) return
+
   return (
     <div className='flex h-screen overflow-hidden relative'>
       <div className='flex-1'>
-        <img src={imageBanner} className='w-full h-full object-cover block' alt='' />
+        <img src={IMAGE_BANNER_LOGIN} className='w-full h-full object-cover block' alt='' />
       </div>
 
-      {/*  */}
       <div className='flex-1 flex items-center '>
         <div className='max-w-[660px] mx-auto px-5'>
           <h1 className='text-7xl font-bold mb-[46px] drop-shadow-xl'>Happening now</h1>
           <h2 className='text-3xl font-bold'>Join today.</h2>
 
           <div className=' max-w-[350px] mt-7'>
-            <OuthLogin></OuthLogin>
+            <OauthLogin />
 
-            <Link to={ROUTER.LOGIN.SIGN_UP} className='btn w-full mt-4 text-white font-medium' type='primary'>
+            <Link
+              to={ROUTER.LOGIN.SIGN_UP}
+              className='btn w-full text-white font-medium bg-primary'
+            >
               Create account
             </Link>
 
             <p className='text-xs mt-2'>
-              By registering, you agree to the <span className='text-primary cursor-pointer'>Terms of Service</span> and{' '}
+              By registering, you agree to the{' '}
+              <span className='text-primary cursor-pointer'>Terms of Service</span> and{' '}
               <span className='text-primary cursor-pointer'>Privacy Policy</span>, including{' '}
               <span className='text-primary cursor-pointer'>Cookie Use.</span>
             </p>
